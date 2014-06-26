103237e0b1871675085beab30dca66b32f1d9aec
itsjustmiles <miles.brownuhs@gmail.com>
2014-06-26 08:02:56 -0700

    Created Intro To Structure App

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String inputReadLine;
+
+            Console.WriteLine("Hello World!");
+            inputReadLine = Console.ReadLine();
+            Console.WriteLine(inputReadLine);
+            Console.ReadLine();
+            
+
+        }
+    }
+}
